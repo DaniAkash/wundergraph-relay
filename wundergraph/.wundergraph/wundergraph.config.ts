36e38512c66efb4e0c112f1af5981a897866c7d6
DaniAkash <s.daniakash@gmail.com>
2023-04-10 18:31:03 +0530

    feat: added operations

--- wundergraph/.wundergraph/wundergraph.config.ts
+++ wundergraph/.wundergraph/wundergraph.config.ts
@@ -1,36 +1,52 @@
-import { configureWunderGraphApplication, cors, EnvironmentVariable, introspect, templates } from '@wundergraph/sdk';
-import server from './wundergraph.server';
-import operations from './wundergraph.operations';
+import {
+  configureWunderGraphApplication,
+  cors,
+  EnvironmentVariable,
+  introspect,
+  templates,
+} from "@wundergraph/sdk";
+import server from "./wundergraph.server";
+import operations from "./wundergraph.operations";
 
-const countries = introspect.graphql({
-	apiNamespace: 'countries',
-	url: 'https://countries.trevorblades.com/',
+const pokemon = introspect.graphql({
+  apiNamespace: "pokemon",
+  url: "https://beta.pokeapi.co/graphql/v1beta",
+});
+
+const starwars = introspect.graphql({
+  apiNamespace: "starwars",
+  url: "https://swapi-graphql.netlify.app/.netlify/functions/index",
 });
 
 // configureWunderGraph emits the configuration
 configureWunderGraphApplication({
-	apis: [countries],
-	server,
-	operations,
-	codeGenerators: [
-		{
-			templates: [
-				// use all the typescript react templates to generate a client
-				...templates.typescript.all,
-			],
-		},
-	],
-	cors: {
-		...cors.allowAll,
-		allowedOrigins:
-			process.env.NODE_ENV === 'production'
-				? [
-						// change this before deploying to production to the actual domain where you're deploying your app
-						'http://localhost:3000',
-				  ]
-				: ['http://localhost:3000', new EnvironmentVariable('WG_ALLOWED_ORIGIN')],
-	},
-	security: {
-		enableGraphQLEndpoint: process.env.NODE_ENV !== 'production' || process.env.GITPOD_WORKSPACE_ID !== undefined,
-	},
+  apis: [pokemon, starwars],
+  server,
+  operations,
+  codeGenerators: [
+    {
+      templates: [
+        // use all the typescript react templates to generate a client
+        ...templates.typescript.all,
+      ],
+    },
+  ],
+  cors: {
+    ...cors.allowAll,
+    allowedOrigins:
+      process.env.NODE_ENV === "production"
+        ? [
+            // change this before deploying to production to the actual domain where you're deploying your app
+            "http://localhost:3000",
+          ]
+        : [
+            "http://localhost:3000",
+            new EnvironmentVariable("WG_ALLOWED_ORIGIN"),
+          ],
+  },
+  security: {
+    enableGraphQLEndpoint:
+      process.env.NODE_ENV !== "production" ||
+      process.env.GITPOD_WORKSPACE_ID !== undefined,
+  },
 });
