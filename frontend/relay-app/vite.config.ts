ebdd09051a96a69b1c618aca95401787c75cf81a
DaniAkash <s.daniakash@gmail.com>
2023-04-12 09:18:39 +0530

    feat: update build scripts

--- frontend/relay-app/vite.config.ts
+++ frontend/relay-app/vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import relay from "vite-plugin-relay";
-import { esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), relay],
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [esbuildCommonjs(["react-relay"])],
-    },
-  },
 });
